4cddaaa08846f8fa974681d82970518f29db38bb
Chrysanthemum Lovelace <chrysanthemum@pitt.edu>
2023-06-07 09:36:49 -0400

    Revert "Remove search link from Not Found"
    This reverts commit cab53989fd87e74b43d25e43c40f4dd361fb9311.

--- src/components/PageNotFound/index.js
+++ src/components/PageNotFound/index.js
@@ -13,6 +13,7 @@ const PageNotFound = () => (
     <div className='not-found'>
       <span className='not-found__icon'><MaterialIcon icon='help_outline' /></span>
       <h1 className='not-found__header'>Sorry, the requested page was not found!</h1>
+      <p className='not-found__text'>Try <a href='/'>a search</a> to find what you're looking for.</p>
     </div>
   </>
 )
