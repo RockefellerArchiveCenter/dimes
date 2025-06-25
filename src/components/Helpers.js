86039b34d37ae2188d30baec54a5a5d87cbe52d8
Hannah <hsistrunk@gmail.com>
2025-06-25 15:55:35 -0400

    update firePageViewEvent to track url changes

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -81,3 +81,22 @@ export const truncateString = (text, maxLength) => {
     return null
   }
 }
+
+/** Sends a custom pageview event to Matomo Tag Manager
+* with pageTitle and pageUrl variables  */
+let lastFiredUrl = null;
+
+export const firePageViewEvent = () => {
+  const currentUrl = window.location.href;
+  // Handle translation, data loading, etc. - if already fired for this URL, skip
+  if (currentUrl === lastFiredUrl) {
+    return;
+  }
+  lastFiredUrl = currentUrl;
+  if (window && window._mtm) {
+    let dataLayer = window._mtm || [];
+    dataLayer.push({
+      event: 'reactPageViewEvent',
+    });
+  }
+}
