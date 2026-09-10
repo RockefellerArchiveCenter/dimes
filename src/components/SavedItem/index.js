416a3055c46f61ff709b285d66476b19a87e09b2
HaSistrunk <hsistrunk@gmail.com>
2026-09-10 10:37:29 -0400

    fix: generate unique link/button labels and use <ul> for mylist items

--- src/components/SavedItem/index.js
+++ src/components/SavedItem/index.js
@@ -6,10 +6,10 @@ import { dateString, truncateString } from '../Helpers'
 import './styles.scss'
 
 const SavedItem = props => (
-  <div className='saved-item mb-40 py-0 pr-22 pl-15'>
+  <li className='saved-item mb-40 py-0 pr-22 pl-15'>
     <div className='saved-item__row'>
       <div className='saved-item__item-description pr-20'>
-        <h3 className='saved-item__title mt-0 mx-0 mb-5'><a href={props.uri}>{props.title}</a></h3>
+        <h3 className='saved-item__title mt-0 mx-0 mb-5' id={`title-${props.uri}`}><a href={props.uri}>{props.title}</a></h3>
         {dateString(props.dates) !== props.title && <p className='saved-item__date'>{dateString(props.dates)}</p>}
         {props.description && <p className='saved-item__description text--truncate'>{truncateString(props.description, 150)}</p>}
         {props.parent && <Trans comment='Saved item found in message'><p className='saved-item__found-in'>Found in: <a href={props.parentRef}>{props.parent}</a></p></Trans>}
@@ -18,15 +18,19 @@ const SavedItem = props => (
       <div className='saved-item__buttons'>
         {props.online &&
           <Trans comment='View Saved Item online'><a className='btn btn--blue btn--sm'
+            id={`view-${props.uri}`}
+            aria-labelledby={`view-${props.uri} title-${props.uri}`}
             href={`${props.uri}/view`}>View Online<MaterialIcon icon='visibility' className='material-icon--space-before'/></a></Trans>}
         <Button
           label={t({ comment: 'Label to remove saved item', message: 'Remove' })}
           className='btn--gray btn--sm'
           iconBefore='delete'
+          id={`remove-${props.uri}`}
+          ariaLabelledBy={`remove-${props.uri} title-${props.uri}`}
           handleClick={props.handleClick} />
       </div>
     </div>
-  </div>)
+  </li>)
 
 const SavedItemGroup = ({ items, removeFromList, title }) => {
   const listItems = items.map((item, index) =>
@@ -40,9 +44,9 @@ const SavedItemGroup = ({ items, removeFromList, title }) => {
   return (
     <div className='saved-items__item-group'>
       <h2 className='item-group__title mt-24 mb-30 p-0'>{title}</h2>
-      <div className='item-group__items'>
+      <ul className='item-group__items list--unstyled'>
         {listItems}
-      </div>
+      </ul>
     </div>
   )
 }
