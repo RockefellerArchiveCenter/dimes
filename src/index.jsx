0e17477f7d86079316aca2d81b6342f829726903
HaSistrunk <hsistrunk@gmail.com>
2026-09-16 16:23:43 -0400

    chore: remove unused CRA service worker

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { I18nApp } from './components/i18n';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<I18nApp ReactComponent={<App />} />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
