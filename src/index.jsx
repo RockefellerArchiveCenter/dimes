162dc435f2eda30664f6a4c2086939a08f11abe8
HaSistrunk <hsistrunk@gmail.com>
2026-09-17 19:00:25 -0400

    refactor: migrate sass from import to the module system

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { I18nApp } from './components/i18n';
+import './styles/styles.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
