ebf24a986db5e96de23e44ac3b540c2286876290
HaSistrunk <hsistrunk@gmail.com>
2026-09-23 10:12:19 -0400

    fix: add MemoryRouter to tests for usePageView

--- src/components/PageNotFound/__tests__/PageNotFound.test.jsx
+++ src/components/PageNotFound/__tests__/PageNotFound.test.jsx
@@ -1,4 +1,5 @@
 import { render, act } from '@testing-library/react'
+import { MemoryRouter } from 'react-router'
 import { I18nApp } from '../../i18n'
 import { t } from '@lingui/core/macro'
 import PageNotFound from '..'
@@ -6,7 +7,10 @@ import PageNotFound from '..'
 it('renders props correctly', () => {
 
   act(() => {
-    render(<I18nApp ReactComponent={<PageNotFound  />} />)
+    render(
+      <MemoryRouter>
+        <I18nApp ReactComponent={<PageNotFound  />} />
+      </MemoryRouter>)
   })
 
   expect(document.querySelector('h1').textContent).toBe(t({
